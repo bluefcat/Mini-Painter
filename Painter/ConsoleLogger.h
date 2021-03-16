c4c1eb26e2e0ad3301c26906d3e0615dcaad65e3
bluefcat <bluefcat@gmail.com>
2021-03-16 21:16:23 +0900

    Deleted Logger code

--- Painter/ConsoleLogger.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Logger.h"
-
-class ConsoleLogger : public Logger {
-public:
-	virtual void info(const std::string&);
-	virtual void waring(const std::string&);
-	virtual void error(const std::string&);
-};
